f677c2866e9e1e7c19f7d925fc9e3ff957d97c1b
Lylixn <46485459+Lylixn@users.noreply.github.com>
2021-11-10 16:21:12 +0100

    Add files via upload

--- jframe.js
+++ jframe.js
@@ -4,27 +4,17 @@ document.body.style.margin = 0
 class Jframe {
 
     //APPLICATION
-    static registerNewApplication (options = {}) { this.applicationInstances.push(new Application(options)) }
-    static getAllApplication () { return this.applicationInstances }
-    static getOpenApplication () {
-
-        let open = []
-
-        for (let app of this.getAllApplication()){
-
-            if (app.checkIfOpen()){
-                open.push(app)
-            }
-
-        }
-
-        return open
+    static registerNewApplication (options = {}) {
+        let instance = new Application(options, this.applicationInstances.length)
+        this.applicationInstances.push(instance)
+        return instance;
     }
+
+    static getAllApplication () { return this.applicationInstances }
     static getApplication (id = 0) { return this.applicationInstances[id] }
 
     //DESKTOP
-    static registerComputer(options = {}) { this.computerInstance = new Computer(options) }
-    static getComputer() { return this.computerInstance }
+    static registerComputer(options = {}) { this.computerInstance =new Computer(options) }
 
     //INSTANCES SAVE
     static applicationInstances = []
@@ -34,18 +24,44 @@ class Jframe {
 
 class Application {
 
-    constructor (options = {}) {
+    static default_icon = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn1.iconfinder.com%2Fdata%2Ficons%2Fhawcons%2F32%2F699297-icon-68-document-file-app-512.png&f=1&nofb=1"
+
+    constructor (options = {}, id) {
 
         this.options = options
-        this.isOpen = 1
+        this.isOpen = false
+        this.id = id
+
+        if (this.options.desktop){
+            Desktop.addApplication(this)
+        }
+    }
+
+    open () {
+        if (this.isOpen){
+            this.focus()
+        } else {
+            Taskbar.registerNewTaskbarApp(this, Jframe.computerInstance.getTaskbar().taskbarUl, Jframe.computerInstance.getTaskbar().options)
+            this.isOpen = true
+        }
+    }
+
+    close () {
+
+    }
+
+    focus ( ) {
 
     }
 
     getOption () { return this.options }
+    getId() {return this.id }
+    getIcon() {return (this.options.icon || Application.default_icon)}
 
     checkIfOpen() {
         return this.isOpen
     }
+
 
 }
 
@@ -64,7 +80,7 @@ class Computer {
         this.computerDiv.style.gridTemplateColumns = '1fr'
         this.computerDiv.style.width = '100vw'
         this.computerDiv.style.height = '100vh'
-        this.computerDiv.style.backgroundColor = this.options.backgroundColor
+        this.computerDiv.style.backgroundColor = (this.options.backgroundColor || "black")
         this.computerDiv.style.backgroundImage = "url("+ this.options.backgroundImg +")"
         this.computerDiv.style.backgroundPosition = "center center"
         this.computerDiv.style.backgroundRepeat = "no-repeat"
@@ -82,12 +98,12 @@ class Computer {
         this.computerDiv.appendChild(this.desktopDiv)
 
         //CREATE DESKTOP INSTANCE
-        this.desktop = new Desktop(this.desktopDiv)
+        this.desktop = new Desktop(this.desktopDiv, this.options)
 
         if (this.options.taskbar){
 
             // STYLE COMPUTER GRID WHEN TASKBAR
-            this.computerDiv.style.gridTemplateRows = '1fr ' + ( this.options.taskbarHeight + 10) + 'px'
+            this.computerDiv.style.gridTemplateRows = '1fr ' + ( ( this.options.taskbarHeight || 60 ) + 10) + 'px'
 
             //CREATE TASKBAR
             this.taskbarDiv = document.createElement("div")
@@ -107,6 +123,8 @@ class Computer {
     }
 
     getOption () { return this.options }
+    getTaskbar () { return this.taskbar }
+    getDesktop () { return this.desktop }
 
 }
 
@@ -131,17 +149,12 @@ class Taskbar {
 
         //APPEND TASKBAR UL TO TASKBAR DIV
         this.taskbarDiv.appendChild(this.taskbarUl)
-
-        for (let app of Jframe.getOpenApplication()){
-            Taskbar.registerNewTaskbarApp(app, this.taskbarUl, this.options)
-        }
-
     }
 
     static registerNewTaskbarApp(application, parent, options){
-
-        this.TaskbarAppInstances.push(new TaskbarItem(application, parent, options))
-
+        if (this.TaskbarAppInstances.length < 30){
+            this.TaskbarAppInstances.push(new TaskbarItem(application, parent, options))
+        }
     }
 
     static TaskbarAppInstances = []
@@ -160,31 +173,31 @@ class TaskbarItem {
 
         //CREATE TASKBAR UL
         let li = document.createElement("li")
-        li.setAttribute("id", this.app.options.appId)
+        li.setAttribute("id", this.app.options.appId + "Taskbar")
         li.style.listStyleType = "none"
         li.style.float = "left"
         li.style.display = "grid"
         li.style.gridTemplateColumns = "25px 10px 25px"
-        li.style.gridTemplateRows = ( this.options.taskbarHeight - 20) + "px 15px 5px"
-        li.style.gridColumnGap = ( this.options.taskbarGap * 2 ) + "px"
+        li.style.gridTemplateRows = ( ( this.options.taskbarHeight || 60 ) - 20) + "px 15px 5px"
+        li.style.gridColumnGap = ( ( this.options.taskbarGap || 0 ) / 2 ) + "px"
         li.style.gridRowGap = '0px'
         li.style.cursor = 'pointer'
         li.style.transition = 'transform 200ms ease-in-out'
 
         let style = document.createElement("style")
-        style.innerHTML = "#"+this.app.options.appId+":hover{transform: scale(1.1);}"
+        style.innerHTML = "#"+ ( this.app.options.appId || app.id ) +"Taskbar:hover{transform: scale(1.1);}"
         document.head.appendChild(style)
 
         let img = document.createElement("img")
-        img.setAttribute("src", app.getOption().icon)
+        img.setAttribute("src", ( app.getIcon()) )
         img.style.gridArea = "1 / 1 / 2 / 4"
 
-        if (this.options.taskbarIconSize > this.options.taskbarHeight) {
-            this.options.taskbarIconSize = this.options.taskbarHeight - 10
+        if (this.options.taskbarIconSize > ( this.options.taskbarHeight || 60 )) {
+            this.options.taskbarIconSize = ( this.options.taskbarHeight || 60 ) - 10
         }
 
-        img.style.width = this.options.taskbarIconSize + "px"
-        img.style.height = this.options.taskbarIconSize + "px"
+        img.style.width = ( this.options.taskbarIconSize || 50 ) + "px"
+        img.style.height = ( this.options.taskbarIconSize || 50 ) + "px"
         img.style.margin = "auto"
         li.appendChild(img)
 
@@ -204,9 +217,81 @@ class TaskbarItem {
 
 class Desktop {
 
-    constructor(desktopDiv) {
+    constructor(desktopDiv, options) {
 
         this.desktopDiv = desktopDiv
+        this.desktopDiv.style.display = "grid"
+        this.desktopDiv.style.margin = "15px"
+        this.desktopDiv.style.gridTemplateColumns = "repeat(" + Math.round((this.desktopDiv.offsetWidth - 30) / (options.desktopIconSize + 10))  + ", "+ (options.desktopIconSize+10) +"px)"
+        this.desktopDiv.style.gridTemplateRows = "repeat(" + Math.round((this.desktopDiv.offsetHeight - 30) / (options.desktopIconSize + 40))  + ", "+ (options.desktopIconSize+40) +"px)"
+    }
+
+    getDesktopDiv () {return this.desktopDiv}
+
+    static addApplication (app) {Desktop.applicationInstances.push(new DesktopItem(app, Jframe.computerInstance.getDesktop().getDesktopDiv()))}
+    static getAllApplication () { return this.applicationInstances }
+    static getApplication (id = 0) { return this.applicationInstances[id] }
+
+    static applicationInstances = []
+
+}
+
+class DesktopItem {
+
+    constructor(app, parent) {
+
+        this.app = app
+
+        this.grid = document.createElement("div")
+        this.grid.setAttribute("id", app.options.appId + "Desktop")
+        this.grid.style.cursor = "pointer"
+        this.grid.style.transition = "background-color 200ms ease-in-out"
+        this.grid.style.overflow = "hidden"
+
+        this.style = document.createElement("style")
+        this.style.innerHTML = "#"+ ( app.options.appId || app.id ) +"Desktop:hover{background-color: rgba(255,255,255,0.3);}" +
+            "#"+ ( app.options.appId || app.id ) +"Desktop:active{transform: scale(0.9);"
+        document.head.appendChild( this.style)
+
+        //CREATE IMG
+        this.img = document.createElement("img")
+        this.img.setAttribute("src", ( app.getIcon()) )
+
+        this.img.style.width = ( (Jframe.computerInstance.getTaskbar().options.desktopIconSize - 10) || 50 ) + "px"
+        this.img.style.height = ( (Jframe.computerInstance.getTaskbar().options.desktopIconSize - 10) || 50 ) + "px"
+        this.img.style.margin = "10px"
+
+        //APPEND TO GRID
+        this.grid.appendChild( this.img)
+
+        //CREATE TXT
+        this.txt = document.createElement("textarea")
+        this.txt.innerText = app.options.title
+        this.txt.style.margin = "0"
+        this.txt.style.fontSize = "13px"
+        this.txt.style.textAlign = "center"
+        this.txt.style.overflow = "hidden"
+        this.txt.style.textOverflow = "ellipsis"
+        this.txt.style.width = "100%"
+        this.txt.style.height = "100%"
+        this.txt.style.outline = "none"
+        this.txt.style.resize = "none"
+        this.txt.style.border = "none"
+        this.txt.style.background = "none"
+        this.txt.style.color = "white"
+        this.grid.appendChild(this.txt)
+
+        //APPEND GRID TO PARENT
+        parent.appendChild( this.grid)
+
+        this.listener()
+    }
+
+    listener () {
+
+        this.grid.addEventListener("click", () => {
+            this.app.open()
+        })
 
     }
 
